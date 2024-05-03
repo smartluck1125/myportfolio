4a4d382f4a5217e95b7560dc5219f740b4f0a2bb
smartluck1125 <167031046+smartluck1125@users.noreply.github.com>
2024-05-03 02:03:33 -0700

    change portfolio file

--- src/portfolio.js
+++ src/portfolio.js
@@ -125,6 +125,7 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+1 (619) 483-0971",
   email_address: "ivan.super.dev@gmail.com"
 };
 
